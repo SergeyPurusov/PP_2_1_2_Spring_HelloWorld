660049d9afd2e24bc6baa887fbae5f84eb67f2b7
SergeyPurusov <purusovsergey@mail.ru>
2023-05-20 01:23:00 +0500

    bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("catBean");
+        Cat cat1 = (Cat) applicationContext.getBean("catBean");
+        System.out.println(cat.getBean());
+        System.out.println(cat1.getBean());
+
+        System.out.println(bean == bean1);
+        System.out.println(cat == cat1);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String bean;
+
+    public String getBean() {
+        return bean;
+    }
+
+    public void setBean(String bean) {
+        this.bean = bean;
+    }
+}
